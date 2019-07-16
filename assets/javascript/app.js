11cf59ff4df8b306bced67337e9006d7e965be82
VJNap <vjnapolitano1364@gmail.com>
2019-07-16 14:54:01 -0500

    VN - Updated radio groups and defined js variables.

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+// Define variables
+
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
+
+// Timer function
+
+
+
+// Reset function
+
+
+
+// Game over function
+
+
+
+// Button interaction
