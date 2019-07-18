ab740e1e17bb89d9c829cf579295336fb2a772f8
VJNap <vjnapolitano1364@gmail.com>
2019-07-18 01:21:05 -0500

    VN - Added JS for the timer, start, and gameover functions. Reorganized radio buttons in an attempt to center them.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,10 +3,36 @@
 var correct = 0;
 var incorrect = 0;
 var unanswered = 0;
-
-// Timer function
-
 console.log(correct);
+
+// Start/Timer function
+
+var number = 120;
+
+var intervalID;
+
+function gameStart() {
+    clearInterval(intervalID);
+    intervalID = setInterval(decrement, 100);
+}
+
+function decrement() {
+    number--;
+
+    $(".timer").html("<h3>Timer: " + number + "</h3>");
+
+    if (number === 0) {
+        stop();
+        timeUp();
+    }
+}
+gameStart();
+
+// Stop function
+
+function stop() {
+    clearInterval(intervalID);
+}
 
 // Reset function
 
@@ -14,6 +40,13 @@ console.log(correct);
 
 // Game over function
 
-
+function timeUp() {
+    console.log("done");
+    $(".container").createElement("div").addClass("gameOverPage").html("<h2>Game Over!</h2>");
+    console.log("time is up");
+}
 
 // Button interaction
+
+    // Take value of the selected radio button
+
